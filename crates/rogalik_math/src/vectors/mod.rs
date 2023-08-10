1f1aee4726d75db29cd27db81f11c4214551572b
maciek.glowka <maciek.glowka@gmail.com>
2023-08-10 20:48:19 +0200

    initial fov

--- crates/rogalik_math/src/vectors/mod.rs
+++ crates/rogalik_math/src/vectors/mod.rs
@@ -2,4 +2,4 @@ pub mod utils;
 pub mod vector2;
 
 pub use vector2::{Vector2F, Vector2I, ORTHO_DIRECTIONS};
-pub use utils::find_path;
+pub use utils::{find_path, visible_tiles};
--- crates/rogalik_math/src/vectors/utils.rs
+++ crates/rogalik_math/src/vectors/utils.rs
@@ -6,6 +6,8 @@ use std::{
 
 use super::vector2::{ORTHO_DIRECTIONS, Vector2I};
 
+// PATH FINDING
+
 pub fn find_path(
     start: Vector2I,
     end: Vector2I,
@@ -65,4 +67,91 @@ impl PartialOrd for Node {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
+}
+
+// FOV
+
+// not heavily tested and rather permissive
+
+pub fn visible_tiles(
+    origin: Vector2I,
+    tiles: &HashSet<Vector2I>,
+    blockers: &HashSet<Vector2I>,
+    range: u32,
+) -> HashSet<Vector2I> {
+    let mut visible = Vec::new();
+
+    for octant in 0..8 {
+        visible.extend(
+            visible_octant(
+                origin,
+                tiles,
+                blockers,
+                range as i32,
+                1,
+                0.0,
+                1.0,
+                octant
+            )
+        );
+    }
+
+    HashSet::from_iter(visible)
+}
+
+fn transform_octant(v: Vector2I, octant: u32) -> Vector2I {
+    match octant {
+        0 => Vector2I::new(v.y, -v.x),
+        1 => Vector2I::new(v.x, -v.y),
+        2 => Vector2I::new(v.x, v.y),
+        3 => Vector2I::new(v.y, v.x),
+        4 => Vector2I::new(-v.y, v.x),
+        5 => Vector2I::new(-v.x, v.y),
+        6 => Vector2I::new(-v.x, -v.y),
+        7 => Vector2I::new(-v.y, -v.x),
+        _ => Vector2I::ZERO
+    }
+}
+
+fn visible_octant(
+    origin: Vector2I,
+    tiles: &HashSet<Vector2I>,
+    blockers: &HashSet<Vector2I>,
+    range: i32,
+    start: i32,
+    mut left_slope: f32,
+    right_slope: f32,
+    octant: u32
+) -> Vec<Vector2I> {
+    let mut visible = vec![origin];
+    if left_slope >= right_slope { return visible };
+
+    for y in start..range {
+        let sx = (y as f32 * left_slope).round() as i32;
+        let ex = (y as f32 * right_slope).round() as i32;
+
+        for x in sx..=ex {
+            let v = origin + transform_octant(Vector2I::new(x, y), octant);
+            if origin.manhattan(v) > range { continue; }
+            if !tiles.contains(&v) { continue; }
+            visible.push(v);
+
+            if !blockers.contains(&v) { continue; }
+            let new_right_slope = (x as f32 - 0.5) / (y as f32 + 0.5);
+            visible.extend(
+                visible_octant(
+                    origin,
+                    tiles,
+                    blockers,
+                    range,
+                    y + 1,
+                    left_slope,
+                    new_right_slope,
+                    octant
+                )
+            );
+            left_slope = (x as f32 + 0.5) / (y as f32 - 0.5);
+        }
+    }
+    visible
 }
