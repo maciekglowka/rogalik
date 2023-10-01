66a5cf77402ef7ad461ebabb8c53e0007c3f66bc
maciek.glowka <maciek.glowka@gmail.com>
2023-10-01 23:50:07 +0200

    wasm changes

--- crates/rogalik_engine/src/wasm.rs
+++ crates/rogalik_engine/src/wasm.rs
@@ -5,20 +5,21 @@ use winit::{
 
 pub fn set_canvas(window: &Window, width: u32, height: u32) {
     use winit::dpi::PhysicalSize;
-        window.set_inner_size(PhysicalSize::new(width, height));
+    window.set_inner_size(PhysicalSize::new(width, height));
 
-        web_sys::window()
-            .and_then(|win| win.document())
-            .and_then(|doc| {
-                let body = doc.get_elements_by_tag_name("body").get_with_index(0)?;
-                let canvas = web_sys::Element::from(window.canvas());
-                body.append_child(&canvas).ok()?;
-                Some(())
-            })
-            .expect("Can't append canvas!");
+    web_sys::window()
+        .and_then(|win| win.document())
+        .and_then(|doc| {
+            let body = doc.get_elements_by_tag_name("body").get_with_index(0)?;
+            let canvas = window.canvas();
+            let canvas_element = web_sys::Element::from(canvas);
+            body.append_child(&canvas_element).ok()?;
+            Some(())
+        })
+        .expect("Can't append canvas!");
 }
 
 pub fn configure_handlers() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-    console_log::init_with_level(log::Level::Warn).expect("Can't init logger!");
+    console_log::init_with_level(log::Level::Warn).expect("Can't init the logger!");
 }
