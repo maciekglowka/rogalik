753ff4110db45ed3b2cbda8f8367814b9987c161
maciek.glowka <maciek.glowka@gmail.com>
2025-07-14 19:23:22 +0200

    readme move

--- crates/rogalik/src/lib.rs
+++ crates/rogalik/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/../../README.md"))]
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "README.md"))]
 
 #[cfg(target_os = "android")]
 mod android;
