616a841dee64396a995e030542fc23b273a7197f
maciek.glowka <maciek.glowka@gmail.com>
2025-07-27 16:28:43 +0200

    generic aabb

--- crates/rogalik_math/src/aabb.rs
+++ crates/rogalik_math/src/aabb.rs
@@ -1,59 +1,44 @@
-use crate::vectors::Vector2f;
+use num_traits::Num;
+
+use crate::vectors::vector2::Vector2;
 
 #[derive(Clone, Copy, Debug, Default)]
-pub struct Aabb {
-    pub a: Vector2f,
-    pub b: Vector2f
+pub struct Aabb<T: Copy + Num> {
+    pub a: Vector2<T>,
+    pub b: Vector2<T>,
 }
-impl Aabb {
-    pub fn new(a: Vector2f, b: Vector2f) -> Self {
-        Self { 
-            a: Vector2f::new(a.x.min(b.x), a.y.min(b.y)),
-            b: Vector2f::new(a.x.max(b.x), a.y.max(b.y)),
+impl<T: Copy + Ord + Num> Aabb<T> {
+    pub fn new(a: Vector2<T>, b: Vector2<T>) -> Self {
+        Self {
+            a: Vector2::<T>::new(a.x.min(b.x), a.y.min(b.y)),
+            b: Vector2::<T>::new(a.x.max(b.x), a.y.max(b.y)),
         }
     }
     pub fn intersects(&self, other: &Self) -> bool {
-        self.a.x < other.b.x &&
-        self.b.x > other.a.x &&
-        self.a.y < other.b.y &&
-        self.b.y > other.a.y
+        self.a.x < other.b.x && self.b.x > other.a.x && self.a.y < other.b.y && self.b.y > other.a.y
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::vectors::Vector2f;
 
     #[test]
     fn intersects_true() {
-        let a = Aabb::new(
-            Vector2f::new(5., 5.),
-            Vector2f::new(10., 10.)
-        );
-        let b = Aabb::new(
-            Vector2f::new(8., 7.),
-            Vector2f::new(20., 30.)
-        );
+        let a = Aabb::new(Vector2f::new(5., 5.), Vector2f::new(10., 10.));
+        let b = Aabb::new(Vector2f::new(8., 7.), Vector2f::new(20., 30.));
         assert!(a.intersects(&b));
     }
     #[test]
     fn intersects_false() {
-        let a = Aabb::new(
-            Vector2f::new(5., 5.),
-            Vector2f::new(10., 10.)
-        );
-        let b = Aabb::new(
-            Vector2f::new(12., 7.),
-            Vector2f::new(20., 30.)
-        );
+        let a = Aabb::new(Vector2f::new(5., 5.), Vector2f::new(10., 10.));
+        let b = Aabb::new(Vector2f::new(12., 7.), Vector2f::new(20., 30.));
         assert!(!a.intersects(&b));
     }
     #[test]
     fn test_coord_swap() {
-        let a = Aabb::new(
-            Vector2f::new(10., 11.),
-            Vector2f::new(5., 6.)
-        );
+        let a = Aabb::new(Vector2f::new(10., 11.), Vector2f::new(5., 6.));
         assert!(a.a.x == 5.);
         assert!(a.a.y == 6.);
         assert!(a.b.x == 10.);
